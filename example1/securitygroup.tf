d7842b8d7087c056f33df68b2052ff630fb3ef6c
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-10-18 18:29:00 -0500

    user

--- example1/securitygroup.tf
+++ example1/securitygroup.tf
@@ -1,21 +1,23 @@
 resource "aws_security_group" "web" {
   name        = "web"
   description = "web"
+  vpc_id      = "vpc-03f46432a850ce405"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -25,6 +27,6 @@ resource "aws_security_group" "web" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "web"
   }
 }
