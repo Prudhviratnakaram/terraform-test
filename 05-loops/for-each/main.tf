16ca8b4bc83dccad291deceb0904cfb947fa44d4
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-10-19 17:16:11 -0500

    user

--- 05-loops/for-each/main.tf
+++ 05-loops/for-each/main.tf
@@ -1,12 +1,29 @@
+/resource "aws_instance" "web" {
+//  for_each      = toset(var.sample)
+//   ami                    = "ami-0da08e4e5b8943a45"
+//  instance_type = "t3.micro"
+//  tags = {
+//    Name = each.key
+//  }
+//}
+//
+//variable "sample" {
+//  default = ["one", "two"]
+//}
+//
 
 resource "aws_instance" "web" {
-  for_each      = toset(var.sample)
-  ami                    = "ami-0da08e4e5b8943a45"
-  instance_type = "t3.micro"
-  tags = {
-    Name = each.key
-  }
+for_each      = var.sample
+ami                    = "ami-0da08e4e5b8943a45"
+instance_type = each.value
+tags = {
+Name = each.key
 }
+}
+
 variable "sample" {
-  default = ["one", "two"]
+default = {
+one = "t3.micro"
+two = "t3.medium"
+}
 }
