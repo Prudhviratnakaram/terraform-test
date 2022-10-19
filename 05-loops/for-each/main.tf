26b804331a0088d9cebb0854900a6dcd944c2b21
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-10-19 17:16:31 -0500

    user

--- 05-loops/for-each/main.tf
+++ 05-loops/for-each/main.tf
@@ -1,4 +1,4 @@
-/resource "aws_instance" "web" {
+//resource "aws_instance" "web" {
 //  for_each      = toset(var.sample)
 //   ami                    = "ami-0da08e4e5b8943a45"
 //  instance_type = "t3.micro"
