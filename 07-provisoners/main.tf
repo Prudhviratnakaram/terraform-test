6fa083193500e3150567cba10b2ce34fdb7ec833
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-10-23 15:15:11 -0500

    user

---
+++ 07-provisoners/main.tf
@@ -0,0 +1,45 @@
+
+//resource "aws_instance" "web" {
+//  ami                    = "ami-07ef4d849950da0b3"
+//  instance_type          = "t3.micro"
+//  vpc_security_group_ids = ["sg-02afc752c16b2fa2e"]
+//
+//  provisioner "remote-exec" {
+//
+//    connection {
+//      host     = self.public_ip
+//      user     = "root"
+//      password = "DevOps321"
+//    }
+//
+//    inline = [
+//      "echo Hello World"
+//    ]
+//
+//  }
+//}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0da08e4e5b8943a45"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e69e1ed431ba73ad"]
+}
+
+resource "null_resource" "cli" {
+  triggers = {
+    abc = timestamp()
+  }
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.web.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello Galaxy"
+    ]
+
+  }
+}
