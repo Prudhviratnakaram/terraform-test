9c75394db7be1358092d12369247158249cc21ea
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2023-04-28 15:01:14 -0500

    user

--- example1/instance.tf
+++ example1/instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0da08e4e5b8943a45"
+  ami                    = "ami-040ef4d5040f158e9"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web.id]
 }
