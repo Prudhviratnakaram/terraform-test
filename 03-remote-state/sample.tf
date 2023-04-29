28bd06644e2645d3455af7c9f7be3f8be0a8d0ae
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2023-04-29 13:07:27 -0500

    user

--- 03-remote-state/sample.tf
+++ 03-remote-state/sample.tf
@@ -7,6 +7,6 @@ terraform {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0da08e4e5b8943a45"
+  ami                    = "ami-040ef4d5040f158e9"
   instance_type          = "t3.micro"
 }
